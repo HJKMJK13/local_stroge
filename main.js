6e632a5325853cf2ac128ef84cc2ca0ee9f059ef
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-17 23:04:23 +0500

    java script started

--- main.js
+++ main.js
@@ -27,13 +27,46 @@
 //clear
 // localStorage.clear()
 
-const names=JSON.parse(localStorage.getItem("names")) ? JSON.parse(localStorage.getItem("names")) :[]
+// const names=JSON.parse(localStorage.getItem("names")) ? JSON.parse(localStorage.getItem("names")) :[]
 
-if(names.length>0){
-    names.forEach((name)=>{
-        console.log(name)
-    })
-}else{
-    console.log("bu yerda hech qanday ism yoq")
+// if(names.length>0){
+//     names.forEach((name)=>{
+//         console.log(name)
+//     })
+// }else{
+//     console.log("bu yerda hech qanday ism yoq")
+// }
+
+function validateForm(){
+    let name=document.getElementById("name").value;
+    let age=document.getElementById("age").value;
+    let address=document.getElementById("address").value;
+    let email=document.getElementById("email").value;
+
+    if(name==""){
+        alert("name is required")
+        return false;
+    }
+
+    if(age ==""){
+        alert("age is required");
+        return false;
+    }
+    else if(age<1){
+        alert("age must not be zero  or less than zero");
+        return false;
+    }
+
+    if(address==""){
+        alert("address is required")
+        return false;
+    }
+
+    if(email ==""){
+        alert("email is required")
+        return false;
+    }
+    else if(email.includes("@")){
+        
+    }
 }
-
