dc3eab03b9b689066718261890302e576cddb7d3
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-17 22:47:25 +0500

    crud application with local storage is started

--- main.js
+++ main.js
@@ -3,19 +3,37 @@
 
 
 //setItem
-let names="Karimjon";
-let surname="Norboyev"
-let age=20
-localStorage.setItem("name",names)
-localStorage.setItem("surname",surname)
-localStorage.setItem("age",age)
+// let names="Karimjon";
+// let surname="Norboyev"
+// let age=20
+// localStorage.setItem("name",names)
+// localStorage.setItem("surname",surname)
+// localStorage.setItem("age",age)
+
+// const names=["Islom","Karim","Burjoq"]
+// localStorage.setItem("name",JSON.stringify(names))
 
 //getItem
-let getAge=localStorage.getItem("age")
-console.log(getAge)
-let getName=localStorage.getItem("name")
-console.log(getName)
-let getSurname=localStorage.getItem("surname")
-console.log(getSurname)
+// let getAge=localStorage.getItem("age")
+// console.log(getAge)
+// let getName=localStorage.getItem("name")
+// console.log(getName)
+// let getSurname=localStorage.getItem("surname")
+// console.log(getSurname)
 
+// const getName=JSON.parse(localStorage.getItem("name"))
+// console.log(getName)
+
+//clear
+// localStorage.clear()
+
+const names=JSON.parse(localStorage.getItem("names")) ? JSON.parse(localStorage.getItem("names")) :[]
+
+if(names.length>0){
+    names.forEach((name)=>{
+        console.log(name)
+    })
+}else{
+    console.log("bu yerda hech qanday ism yoq")
+}
 
