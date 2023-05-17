8d5dc22d5ab109cefc0f130f9a1caa1c582a3ad0
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-17 21:57:46 +0500

    getItem haqida bilib oldik

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 // console.log(localStorage.getItem("name"))
 // localStorage.setItem("name","salom")
+
+
+//setItem
 let names="Karimjon";
 let surname="Norboyev"
 let age=20
 localStorage.setItem("name",names)
 localStorage.setItem("surname",surname)
 localStorage.setItem("age",age)
+
+//getItem
+let getAge=localStorage.getItem("age")
+console.log(getAge)
+let getName=localStorage.getItem("name")
+console.log(getName)
+let getSurname=localStorage.getItem("surname")
+console.log(getSurname)
+
 
