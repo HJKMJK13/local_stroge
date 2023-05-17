baf2f8cad176f72a429646743bfb461064966f6f
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-17 21:52:33 +0500

    setItem haqida bilimlarga ega boldik

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-console.log(localStorage.getItem("name"))
+// console.log(localStorage.getItem("name"))
 // localStorage.setItem("name","salom")
+let names="Karimjon";
+let surname="Norboyev"
+let age=20
+localStorage.setItem("name",names)
+localStorage.setItem("surname",surname)
+localStorage.setItem("age",age)
+
